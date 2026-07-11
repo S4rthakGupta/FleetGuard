f0fe394ff03e20f81b4641ddb0f4b6dcb6236dab
S4rthakGupta <sarthakgupta9814@gmail.com>
2026-07-11 00:28:00 -0400

    feat: implement device registration endpoint

---
+++ Enums/DevicePlatform.cs
@@ -0,0 +1,12 @@
+﻿namespace FleetGuard.Enums
+{
+    public enum DevicePlatform
+    {
+        Android=1,
+        iOS = 2,
+        Windows = 3,
+        Linux = 4,
+        Printer = 5,
+        Other = 6
+    }
+}
