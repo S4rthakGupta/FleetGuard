1df71c5d7891a0f79d78eb30dcebac01aad07eae
S4rthakGupta <sarthakgupta9814@gmail.com>
2026-07-11 05:49:17 -0400

    feat: add device update and delete endpoints

---
+++ Requests/UpdateDeviceRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FleetGuard.Enums;
+
+namespace FleetGuard.Requests
+{
+    public class UpdateDeviceRequest
+    {
+        [Required]
+        public string DeviceName { get; set; } = string.Empty;
+
+        [Required]
+        public string SerialNumber { get; set; } = string.Empty;
+
+        [Required]
+        public DevicePlatform Platform { get; set; }
+
+        [Required]
+        public string OperatingSystemVersion { get; set; } = string.Empty;
+
+        [Required]
+        public DeviceStatus Status { get; set; }
+    }
+}
